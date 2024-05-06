9533d764ab202a06348eb5bea1d494efbdcf466f
limjunan <bryanlim080302@gmail.com>
2024-05-07 02:28:25 +0800

    070524 0224H

--- Arrays-Hashing/lc/sliding_window_maximum.py
+++ Arrays-Hashing/lc/sliding_window_maximum.py
@@ -23,3 +23,31 @@ class Solution:
                 output.append(max(nums[i-k+1:i+1]))
 
         return output
+    
+# Submitted Solution 2
+# Using deque
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if k == len(nums):
+            return [max(nums)]
+        
+        q = collections.deque()
+        output = []
+        l, r = 0, 0
+
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+
+            r += 1
+
+        return output
+    
